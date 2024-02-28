28e8728383557dfd07b9735bfcc064eecabacc91
shockpast <shockpast@gmail.com>
2024-02-28 18:13:19 +0300

    feat: "screengrabbing" notification

--- hooks/RenderView.h
+++ hooks/RenderView.h
@@ -4,11 +4,18 @@
 
 #include "../features/gui/gui.h"
 
+#include "../helpers/common.h"
+
 typedef bool(__thiscall* _RenderView)(CViewRender*, CViewSetup&, int, int);
 _RenderView oRenderView;
 
 // https://www.unknowncheats.me/forum/2378329-post1.html
 bool __fastcall hkRenderView(CViewRender* ViewRender, CViewSetup& view, int nClearFlags, int whatToDraw)
 {
+	if (EngineClient->IsTakingScreenshot())
+	{
+		logger::AddLog("[warning] screengrabbed -> [screenshot] @ %s", GetTime());
+	}
+
 	return oRenderView(ViewRender, view, nClearFlags, whatToDraw);
 }
